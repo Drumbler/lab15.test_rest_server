050869ec2a080e3f23e1d200cd9451eb2755f415
Andrey <andy132012@gmail.com>
2023-06-07 21:37:42 +0500

    initial commit

--- src/main/java/com/example/test_rest_server/TestRestServerApplication.java
+++ src/main/java/com/example/test_rest_server/TestRestServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestRestServerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TestRestServerApplication.class, args);
     }
 
